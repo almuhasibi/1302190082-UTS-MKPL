4e23a0a99a3058ebbf0c61d2914a42e6ca32de6a
almuhasibi <zahidaja11@gmail.com>
2022-04-20 18:57:00 +0700

    Implementasi Large Class Untuk memindahkan Atrtibute dan variable pada Class Semestinya

---
+++ TaxCalculator/src/lib/EmployeeSalary.java
@@ -0,0 +1,49 @@
+package lib;
+
+public class EmployeeSalary extends Employee{
+
+    private int monthSalary;
+	private int otherMonthlyIncome;
+	private int annualDeductible;
+
+    public EmployeeSalary(Employee worker){
+        super(worker.getFirstName(), worker.getLastName(), worker.getEmployeeId(), 
+        worker.getAddress(), worker.getForeigner(), worker.getGender());
+    }
+
+    public void setmonthSalary(int grade) {	
+		if (grade == 1) {
+			monthSalary = 3000000;
+			foreignerSalary(monthSalary);
+		}else if (grade == 2) {
+			monthSalary = 5000000;
+			foreignerSalary(monthSalary);
+		}else if (grade == 3) {
+			monthSalary = 7000000;
+			foreignerSalary(monthSalary);
+		}
+        
+	}
+    public int getMonthSalary(){ 
+        return monthSalary;
+    }
+ 
+    public void foreignerSalary(int monthSalary){  
+    }
+	
+	public void setAnnualDeductible(int deductible) { 
+		this.annualDeductible = deductible;
+	}
+	
+	public void setAdditionalIncome(int income) {	 
+		this.otherMonthlyIncome = income;
+	}
+
+    public int getAnnualDeductible(){ 
+        return annualDeductible;
+    }
+
+    public int getAddtionalIncome(){ 
+        return otherMonthlyIncome;
+    }
+}
