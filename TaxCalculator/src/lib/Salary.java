b5f6a8831be54801fd89cbabdc296b2b9a567a5b
almuhasibi <zahidaja11@gmail.com>
2022-04-20 19:15:16 +0700

    Temporary Field & Long Method pada class function Tax

---
+++ TaxCalculator/src/lib/Salary.java
@@ -0,0 +1,13 @@
+package lib;
+
+public class Salary {
+
+    public int getMonthlySalary() {
+        return 0;
+    }
+
+    public int getOtherMonthlyIncome() {
+        return 0;
+    }
+
+}
--- TaxCalculator/src/lib/TaxFunction.java
+++ TaxCalculator/src/lib/TaxFunction.java
@@ -1,24 +1,10 @@
 package lib;
 
-public class TaxFunction {
+public class TaxFunction {	
 
-	
-	/**
-	 * Fungsi untuk menghitung jumlah pajak penghasilan pegawai yang harus dibayarkan setahun.
-	 * 
-	 * Pajak dihitung sebagai 5% dari penghasilan bersih tahunan (gaji dan pemasukan bulanan lainnya dikalikan jumlah bulan bekerja dikurangi pemotongan) dikurangi penghasilan tidak kena pajak.
-	 * 
-	 * Jika pegawai belum menikah dan belum punya anak maka penghasilan tidak kena pajaknya adalah Rp 54.000.000.
-	 * Jika pegawai sudah menikah maka penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000.
-	 * Jika pegawai sudah memiliki anak maka penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000 per anak sampai anak ketiga.
-	 * 
-	 */
-	
-	
-	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
-		
+	public static int calculateTax(EmployeeSalary salary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
+
 		int tax = 0;
-		
 		if (numberOfMonthWorking > 12) {
 			System.err.println("More than 12 month working per year");
 		}
@@ -28,9 +14,9 @@ public class TaxFunction {
 		}
 		
 		if (isMarried) {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
+			tax = (int) Math.round(0.05 * (((otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
 		}else {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
+			tax = (int) Math.round(0.05 * (((otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
 		}
 		
 		if (tax < 0) {
@@ -40,5 +26,15 @@ public class TaxFunction {
 		}
 			 
 	}
-	
+	public static int sumIncome(Salary salary, int MonthWorking) {
+		return ((salary.getMonthlySalary() + salary.getMonthlySalary()) * MonthWorking);
+	}
+
+	public static int amountOfTaxPay(int tax){
+		if (tax < 0) {
+			return 0;
+		}else {
+			return tax;
+		}
+	}
 }
